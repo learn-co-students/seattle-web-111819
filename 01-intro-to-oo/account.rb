c0dc3148e31ba61716d651bf7008b8a32e000adf
Flatiron School <soundarya.rajendran@flatironschool.com>
2019-11-19 12:35:20 -0800

    Adding oo code

---
+++ 01-intro-to-oo/account.rb
@@ -0,0 +1,71 @@
+#pulling in resource (rubygems.org), and uses in our program
+require 'pry'
+
+bank_accounts = [{"owner_name": "soundarya", "balance": 100, due_date: "12/25/2019"},
+                {owner_name: "Chris", balance: 1000, due_date: "12/25/2019"},
+                {owner_name: "Natali", balance: 3000, due_date: "12/25/2019"},
+                {owner_name: "Jacob", balance: 500, due_date: "12/25/2019"},
+                {owner_name: "Amy", balance: 2000, due_date: "12/25/2019"}
+            ]
+
+
+
+# Question asked: what languages should I learn after school.
+
+# another sample class
+# class House
+#     def initialize()
+#         @kitchen
+#         @color
+#         @stories
+#         total_length = stories * 5.6
+#         @room_number
+#     end
+# end
+
+class BankAccount
+    attr_reader(:owner_name, :due_date) #(41-52)
+    attr_accessor(:balance) #(55-57)
+    #attr_accessor which does both
+
+    def initialize(owner_name, balance, due_date)
+        # local variable.
+        #scope? go through certain levels.
+        # global, local -> 
+        # access 
+        @owner_name = owner_name
+        @balance = balance
+        @due_date = due_date
+        # binding.pry
+    end
+
+    #return the variable to the user
+    # def owner_name
+    #     @owner_name
+    # end
+
+    # def balance
+    #     @balance
+    # end
+
+    # def due_date
+    #     @due_date
+    # end
+
+    #these are writer methods
+    # def balance=(new_balance)
+    #     @balance = new_balance
+    # end
+end
+# owner_name: string
+# balance: integer
+# due_date: string
+
+bank_accounts.each do |bank_account| 
+    # binding.pry
+    BankAccount.new(bank_account.values[0],bank_account.values[1], bank_account.values[2] )
+end
+
+soundarya = BankAccount.new("Soundatya", 1000, "11/11/11" )
+
+binding.pry
